fa48e1d9643eb8d4154c46394f7568750a830416
newming <newming1993@gmail.com>
2018-05-12 14:23:32 +0800

    change pwd

--- server/db/index.js
+++ server/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const config = require('../config')
 
 let db = config.dbProd
-
+// mongodb pwd 不可以包含部分特殊符号 =, @, $
 if (process.env.NODE_ENV !== 'production') {
   mongoose.set('debug', true)
   db = config.dbDev
